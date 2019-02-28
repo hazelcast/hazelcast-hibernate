90fc88aefb85582c35ea49a99b00bf920cff0db4
dohertyfjatl <44657799+dohertyfjatl@users.noreply.github.com>
2019-03-01 01:56:55 +1100

    Fix for #55 - entries evicted too quickly when ttl is greater than zero (#56)
    * Fix for #55 - entries evicted too quickly when ttl is greater than zero
    * reverted LocalRegionCache to use ``HazelcastTimestamper.nextTimestamp`` (as Hibernate's ``SimpleTimestamper`` is not
      returning values in milliseconds, which is required for the expiry calculation)
    * added test cases for this scenario to ``RegionFactoryQueryCacheEvictionSlowTest``
    * changed ``CleanupService`` to allow the ``fixedDelay`` to be configurable, for use in the above tests

--- hazelcast-hibernate5/src/main/java/com/hazelcast/hibernate/local/CleanupService.java
+++ hazelcast-hibernate5/src/main/java/com/hazelcast/hibernate/local/CleanupService.java
@@ -29,13 +29,24 @@ import java.util.concurrent.TimeUnit;
  */
 public final class CleanupService {
 
-    private static final long FIXED_DELAY = 60;
-    private static final long FIXED_DELAY1 = 60;
+    /**
+     * Default fixed delay in seconds for scheduled job.
+     */
+    private static final long DEFAULT_FIXED_DELAY = 60L;
 
+    private final long fixedDelay;
     private final String name;
     private final ScheduledExecutorService executor;
 
     public CleanupService(final String name) {
+        this(name, DEFAULT_FIXED_DELAY);
+    }
+
+    /**
+     * Visible for testing only.
+     */
+    public CleanupService(final String name, final long fixedDelay) {
+        this.fixedDelay = fixedDelay;
         this.name = name;
         executor = Executors.newSingleThreadScheduledExecutor(new CleanupThreadFactory());
     }
@@ -47,7 +58,7 @@ public final class CleanupService {
             public void run() {
                 cache.cleanup();
             }
-        }, FIXED_DELAY, FIXED_DELAY1, TimeUnit.SECONDS);
+        }, fixedDelay, fixedDelay, TimeUnit.SECONDS);
     }
 
     public void stop() {
