ad48e7e1fb00dbb11566ea07bfe6fb1ba5b5dacf
hasancelik <hasan@hazelcast.com>
2019-06-11 15:52:37 +0300

    fixed test failures

--- hazelcast-hibernate53/src/test/java/com/hazelcast/hibernate/RegionFactoryQueryCacheEvictionSlowTest.java
+++ hazelcast-hibernate53/src/test/java/com/hazelcast/hibernate/RegionFactoryQueryCacheEvictionSlowTest.java
@@ -20,6 +20,7 @@ import com.hazelcast.config.MapConfig;
 import com.hazelcast.hibernate.local.TestLocalCacheRegionFactory;
 import com.hazelcast.hibernate.region.TestCacheRegionFactory;
 import com.hazelcast.test.HazelcastParametersRunnerFactory;
+import com.hazelcast.test.HazelcastSerialParametersRunnerFactory;
 import com.hazelcast.test.annotation.SlowTest;
 import org.hibernate.cache.spi.support.QueryResultsRegionTemplate;
 import org.hibernate.cfg.Environment;
@@ -37,7 +38,7 @@ import static com.hazelcast.hibernate.local.TestLocalCacheRegionFactory.CLEANUP_
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
-@Parameterized.UseParametersRunnerFactory(HazelcastParametersRunnerFactory.class)
+@Parameterized.UseParametersRunnerFactory(HazelcastSerialParametersRunnerFactory.class)
 @Category(SlowTest.class)
 public class RegionFactoryQueryCacheEvictionSlowTest extends HibernateSlowTestSupport {
 
