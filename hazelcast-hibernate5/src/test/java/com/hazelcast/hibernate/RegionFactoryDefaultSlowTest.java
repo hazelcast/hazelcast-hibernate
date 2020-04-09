0acddf1108282acd90b0bc5c5ab68217b30230ee
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-04-09 07:28:42 +0200

    Remove eviction test

--- hazelcast-hibernate5/src/test/java/com/hazelcast/hibernate/RegionFactoryDefaultSlowTest.java
+++ hazelcast-hibernate5/src/test/java/com/hazelcast/hibernate/RegionFactoryDefaultSlowTest.java
@@ -15,57 +15,29 @@
 
 package com.hazelcast.hibernate;
 
-import com.hazelcast.config.MapConfig;
 import com.hazelcast.hibernate.entity.DummyEntity;
-import com.hazelcast.hibernate.region.HazelcastQueryResultsRegion;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.SlowTest;
-import org.awaitility.Awaitility;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Environment;
-import org.hibernate.internal.SessionFactoryImpl;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
-import static org.awaitility.Awaitility.await;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(SlowTest.class)
 public class RegionFactoryDefaultSlowTest
-        extends HibernateSlowTestSupport {
+  extends HibernateSlowTestSupport {
 
     protected Properties getCacheProperties() {
         Properties props = new Properties();
         props.setProperty(Environment.CACHE_REGION_FACTORY, HazelcastCacheRegionFactory.class.getName());
         return props;
-    }
-
-    @Test
-    public void testQueryCacheCleanup() {
-
-        MapConfig mapConfig = getHazelcastInstance(sf).getConfig().getMapConfig("org.hibernate.cache.*");
-        final float baseEvictionRate = 0.2f;
-        final int numberOfEntities = 100;
-        final int defaultCleanupPeriod = 60;
-        final int maxSize = mapConfig.getEvictionConfig().getSize();
-        final int evictedItemCount = numberOfEntities - maxSize + (int) (maxSize * baseEvictionRate);
-        insertDummyEntities(numberOfEntities);
-        for (int i = 0; i < numberOfEntities; i++) {
-            executeQuery(sf, i);
-        }
-
-        HazelcastQueryResultsRegion queryRegion = ((HazelcastQueryResultsRegion) (((SessionFactoryImpl) sf).getQueryCache()).getRegion());
-        assertEquals(numberOfEntities, queryRegion.getCache().size());
-
-        await()
-          .atMost(defaultCleanupPeriod + 1, TimeUnit.SECONDS)
-          .until(() -> (numberOfEntities - evictedItemCount) == queryRegion.getCache().size());
     }
 
     @Test
