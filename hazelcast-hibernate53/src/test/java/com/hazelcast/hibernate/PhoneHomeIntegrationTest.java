4fae6b916f590e954178e8e4514b13c80f44de3e
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2023-06-07 12:21:19 +0200

    setup wiremock server

--- hazelcast-hibernate53/src/test/java/com/hazelcast/hibernate/PhoneHomeIntegrationTest.java
+++ hazelcast-hibernate53/src/test/java/com/hazelcast/hibernate/PhoneHomeIntegrationTest.java
@@ -8,13 +8,17 @@ import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static org.awaitility.Awaitility.await;
 
 public class PhoneHomeIntegrationTest {
 
-    private WireMockServer wireMockServer = new WireMockServer();
+    private final WireMockServer wireMockServer = new WireMockServer();
 
     @Before
     public void setup() {
@@ -25,13 +29,17 @@ public class PhoneHomeIntegrationTest {
     @Test
     public void phoneHomeRequestTest() {
         PhoneHomeInfo info = new PhoneHomeInfo(true);
-        PhoneHomeService service = new PhoneHomeService("http://127.0.0.1:" + wireMockServer.port() +
+        PhoneHomeService service = new PhoneHomeService("http://127.0.0.1:8080" +
                 "/hazelcast-hibernate53", info);
+        String url = "/hazelcast-hibernate53" + info.getQueryString();
+
+        stubFor(get(urlEqualTo(url)).willReturn(aResponse().withStatus(HTTP_NOT_FOUND)));
+
         service.start();
 
         await().atMost(30, TimeUnit.SECONDS).untilAsserted(() ->
                 // verify 5 retries
-                WireMock.verify(5, getRequestedFor(urlEqualTo("/hazelcast-hibernate53" + info.getQueryString())))
+                WireMock.verify(5, getRequestedFor(urlEqualTo(url)))
         );
 
         service.shutdown();
